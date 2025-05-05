b684c86c489d9f0180205feccffee4764114483f
Frank Bria <frank.bria@pm.me>
2025-05-05 09:08:40 -0700

    Add icons and metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,16 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/favicon.ico" />
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="180x180" href="/favicon-180x180.png" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="description" content="Red Golden Retriever Stud Services in Arizona" />
+        <meta name="keywords" content="Cave Creek Goldens, Red Golden Retrievers, Stud Services, Arizona, Golden Retriever" />
+        <meta name="author" content="Noatak Enterprises, LLC, dba Cave Creek Goldens" />
+      </head>
       <body className={`${inter?.className || ''} bg-gray-50 text-gray-800 antialiased flex flex-col min-h-screen`}>
         <header className="sticky top-0 z-50">
           <Navbar />
